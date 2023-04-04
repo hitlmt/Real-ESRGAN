84dd33d75defa96e635366b1951a9da41ffb9245
limt <limt@zkt.cn>
2023-04-04 15:22:42 +0800

    update

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ def main() -> int:
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = RealESRGAN(device, scale=scale)
     model.load_weights(f'weights/RealESRGAN_x{scale}.pth', download=True)
-    for i, image in enumerate(os.listdir("inputs")):
-        image = Image.open(f"inputs/{image}").convert('RGB')
+    for i, image_name in enumerate(os.listdir("inputs")):
+        image = Image.open(f"inputs/{image_name}").convert('RGB')
         t0 = time.time()
         sr_image = model.predict(image)
         t = time.time() - t0
         print(f"### {t}s")
-        sr_image.save(f'results/{image}_{scale}.png')
+        sr_image.save(f'results/{image_name.split(".")[0]}_{scale}.png')
 
 
 if __name__ == '__main__':
